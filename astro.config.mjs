2dd96d64a16bada37b4440b0e4a6464a32b6b68a
Batman <ellwil@shodh.ai>
2025-12-26 18:37:11 +0530

    updated the hindi and english

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://save-the-aravalis.example',
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
